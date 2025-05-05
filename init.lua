3f594f6a8fa352927e6fa68abbfcca18b3d7a62f
profpyrus <profpyrus@gmail.com>
2025-05-05 22:11:07 +0200

    add diagnostics text at end of line

--- init.lua
+++ init.lua
@@ -652,6 +652,10 @@ require('lazy').setup({
         end
       end
 
+      vim.diagnostic.config {
+        virtual_text = true,
+      }
+
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
