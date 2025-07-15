a0f12b8efe189579c678ce2be0baca0063912aa2
profpyrus <profpyrus@gmail.com>
2025-07-15 10:49:24 +0200

    rely more on mini.nvim

--- init.lua
+++ init.lua
@@ -96,17 +96,6 @@ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 -- Neotree mappings
 vim.keymap.set('n', '\\', ':Neotree toggle<CR>')
 
--- Bufferline mappings
-vim.keymap.set('n', '<A-h>', ':BufferLineMovePrev<CR>')
-vim.keymap.set('n', '<A-l>', ':BufferLineMoveNext<CR>')
-vim.keymap.set('n', '<A-j>', ':BufferLineCyclePrev<CR>')
-vim.keymap.set('n', '<A-k>', ':BufferLineCycleNext<CR>')
-vim.keymap.set('n', 'gb', ':BufferLinePick<CR>', { desc = '[G]oto [B]uffer' })
-
-vim.keymap.set('n', '<A-p>', ':BufferLineTogglePin<CR>')
-
-vim.keymap.set('n', '<A-d>', ':bp|bd! #<CR>', { desc = '[D]elete current buffer' })
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -157,46 +146,6 @@ require('lazy').setup({
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      require('mini.notify').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      -- local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      -- statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      -- statusline.section_location = function()
-      -- return '%2l:%-2v'
-      -- end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
 
   { import = 'plugins' },
 }, {
