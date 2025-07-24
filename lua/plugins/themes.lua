8a183a4d9d4d71af42ea50abfadc96981b100f9f
profpyrus <profpyrus@gmail.com>
2025-07-24 11:47:40 +0200

    actually fix use of opts and make background actually transparent

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,68 +1,68 @@
 return {
-  {
-    'Mofiqul/dracula.nvim',
-    name = 'dracula',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      ---@type dracula
-      local dracula = require 'dracula'
-
-      dracula.setup {
-        styles = {
-          Type = {},
-          Function = {},
-          Parameter = {},
-          Property = {},
-          Comment = {},
-          String = {},
-          Keyword = {},
-          Identifier = {},
-          Constant = {},
-        },
-        transparent = true,
-        on_colors = function(colors, color)
-          ---@type dracula.palette
-          return {
-            -- override or create new colors
-            -- mycolor = '#ffffff',
-            -- mycolor = 0xffffff,
-          }
-        end,
-        -- on_highlights = function(colors, color)
-        --   ---@type dracula.highlights
-        --   return {
-        --     ---@type vim.api.keyset.highlight
-        --     Normal = { fg = colors.mycolor },
-        --   }
-        -- end,
-        plugins = {
-          ['nvim-treesitter'] = true,
-          -- ['rainbow-delimiters'] = true,
-          ['nvim-lspconfig'] = true,
-          -- ['nvim-navic'] = true,
-          ['nvim-cmp'] = true,
-          ['indent-blankline.nvim'] = true,
-          ['neo-tree.nvim'] = true,
-          -- ['nvim-tree.lua'] = true,
-          ['which-key.nvim'] = true,
-          -- ['dashboard-nvim'] = true,
-          ['gitsigns.nvim'] = true,
-          ['neogit'] = true,
-          ['todo-comments.nvim'] = true,
-          ['lazy.nvim'] = true,
-          ['telescope.nvim'] = true,
-          ['noice.nvim'] = true,
-          ['hop.nvim'] = true,
-          ['mini.statusline'] = true,
-          ['mini.tabline'] = true,
-          ['mini.starter'] = true,
-          ['mini.cursorword'] = true,
-          ['bufferline.nvim'] = true,
-        },
-      }
-    end,
-  },
+  -- {
+  --   'Mofiqul/dracula.nvim',
+  --   name = 'dracula',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     ---@type dracula
+  --     local dracula = require 'dracula'
+  --
+  --     dracula.setup {
+  --       styles = {
+  --         Type = {},
+  --         Function = {},
+  --         Parameter = {},
+  --         Property = {},
+  --         Comment = {},
+  --         String = {},
+  --         Keyword = {},
+  --         Identifier = {},
+  --         Constant = {},
+  --       },
+  --       transparent = true,
+  --       on_colors = function(colors, color)
+  --         ---@type dracula.palette
+  --         return {
+  --           -- override or create new colors
+  --           -- mycolor = '#ffffff',
+  --           -- mycolor = 0xffffff,
+  --         }
+  --       end,
+  --       -- on_highlights = function(colors, color)
+  --       --   ---@type dracula.highlights
+  --       --   return {
+  --       --     ---@type vim.api.keyset.highlight
+  --       --     Normal = { fg = colors.mycolor },
+  --       --   }
+  --       -- end,
+  --       plugins = {
+  --         ['nvim-treesitter'] = true,
+  --         -- ['rainbow-delimiters'] = true,
+  --         ['nvim-lspconfig'] = true,
+  --         -- ['nvim-navic'] = true,
+  --         ['nvim-cmp'] = true,
+  --         ['indent-blankline.nvim'] = true,
+  --         ['neo-tree.nvim'] = true,
+  --         -- ['nvim-tree.lua'] = true,
+  --         ['which-key.nvim'] = true,
+  --         -- ['dashboard-nvim'] = true,
+  --         ['gitsigns.nvim'] = true,
+  --         ['neogit'] = true,
+  --         ['todo-comments.nvim'] = true,
+  --         ['lazy.nvim'] = true,
+  --         ['telescope.nvim'] = true,
+  --         ['noice.nvim'] = true,
+  --         ['hop.nvim'] = true,
+  --         ['mini.statusline'] = true,
+  --         ['mini.tabline'] = true,
+  --         ['mini.starter'] = true,
+  --         ['mini.cursorword'] = true,
+  --         ['bufferline.nvim'] = true,
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
@@ -72,8 +72,5 @@ return {
       flavour = 'mocha',
       transparent_background = true,
     },
-    config = function()
-      vim.cmd.colorscheme 'catppuccin'
-    end,
   },
 }
